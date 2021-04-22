de43c3594215c339958fb27d299b281841e7828e
Daniil <dshwets@gmail.com>
2021-04-23 01:00:53 +0600

    #1 Создана модель сделок

---
+++ source/api_v1/models.py
@@ -0,0 +1,33 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from common.models import AbstractDatetimeModel
+
+
+class Deals(AbstractDatetimeModel):
+    customer = models.CharField(
+        max_length=100,
+        verbose_name=_("Клиент"),
+    )
+    item = models.CharField(
+        max_length=100,
+        verbose_name=_("Предмет"),
+    )
+    total = models.IntegerField(
+        validators=[MinValueValidator(0), ],
+        verbose_name=_("Потраченная сумма"),
+
+    )
+    qty = models.IntegerField(
+        validators=[MinValueValidator(0), ],
+        verbose_name=_("Количество"),
+
+    )
+    date_time = models.DateTimeField(
+        verbose_name=_("Дата"),
+    )
+
+    class Meta:
+        verbose_name = _('Сделка')
+        verbose_name_plural = _('Сделки')
