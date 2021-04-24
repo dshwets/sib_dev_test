4b0bf7018f449dd23ab5425f2871b89fe988b5f0
Daniil <dshwets@gmail.com>
2021-04-24 16:13:05 +0600

    #1 Реализовал метод для получения списка клиентов.

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,6 +1,10 @@
 import csv
 import io
 
+from datetime import timedelta
+
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Sum
 from django.utils.dateparse import parse_datetime
 from django.utils.timezone import make_aware
 
@@ -8,8 +12,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api_v1.models import Deals
-from api_v1.serializers import FileUploadSerializer
+from api_v1.models import Deal
+from api_v1.serializers import FileUploadSerializer, TopClientSerializer
 
 
 class UploadDealsView(APIView):
@@ -44,12 +48,33 @@ class UploadDealsView(APIView):
             if date is None or total < 0 or qty < 0:
                 wrong_strings["invalid_data_strings:"] += str(reader.line_num) + " "
                 continue
-            Deals_objects.append(Deals(
+            Deals_objects.append(Deal(
                 customer=line[0],
                 item=line[1],
                 total=line[2],
                 qty=line[3],
                 date_time=make_aware(date),
             ))
-        Deals.objects.bulk_create(Deals_objects)
-        return Response("data is uploaded correct" if not wrong_strings else wrong_strings, status=status.HTTP_200_OK)
+        Deal.objects.bulk_create(Deals_objects)
+        return Response("data is uploaded correct" if not wrong_strings['invalid_data_strings:'] else wrong_strings, status=status.HTTP_200_OK)
+
+
+class GetTopCustomers(APIView):
+    def get(self, request, *args, **kwargs):
+        last_request_time = Deal.objects.last().created_at
+        last_request_time_delta = last_request_time - timedelta(seconds=1)
+        all_clients = Deal.objects.filter(created_at__range=(last_request_time_delta,
+                                                             last_request_time))
+        top_5_clients = all_clients.values('customer').annotate(gems=ArrayAgg('item', distinct=True),
+                                                                total=Sum('total')).order_by('-total')[:5]
+        for client in top_5_clients:
+            same_gems = set()
+            for _ in top_5_clients:
+                gems_set = set(client['gems'])
+                if client == _:
+                    continue
+                gems_set.intersection_update(_['gems'])
+                same_gems.update(gems_set)
+            client['gems'] = list(same_gems)
+        slr = TopClientSerializer(top_5_clients, many=True)
+        return Response({"response": slr.data}, 200)
