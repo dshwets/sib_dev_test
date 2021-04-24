9f239405b13de7423cd6e0d58de9bd615e73d5f0
Daniil <dshwets@gmail.com>
2021-04-24 16:13:36 +0600

    #1 Добавил в урлы путь для получения клиентов.

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from api_v1.views import UploadDealsView
+from api_v1.views import UploadDealsView, GetTopCustomers
 
 app_name = 'api_v1'
 
 urlpatterns = [
     path('api_v1/upload_deals', UploadDealsView.as_view(), name='upload_deals'),
+    path('api_v1/get_top_customers', GetTopCustomers.as_view(), name='get_top_customers'),
 ]
