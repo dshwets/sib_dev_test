186e8db51fc840e2b1ca47f23266aab9b54dd384
Daniil <dshwets@gmail.com>
2021-04-25 11:37:38 +0600

    #1 Изменил юрлы на корректные.

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -5,6 +5,6 @@ from api_v1.views import UploadDealsView, GetTopCustomers
 app_name = 'api_v1'
 
 urlpatterns = [
-    path('api_v1/upload_deals', UploadDealsView.as_view(), name='upload_deals'),
-    path('api_v1/get_top_customers', GetTopCustomers.as_view(), name='get_top_customers'),
+    path('api_v1/deals', UploadDealsView.as_view(), name='upload_deals'),
+    path('api_v1/top_customers', GetTopCustomers.as_view(), name='get_top_customers'),
 ]
